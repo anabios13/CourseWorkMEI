a60e8048c124540746b2976510dd09b76c4602c1
anabios13 <o.pinchykov@mail.ru>
2023-05-12 14:21:45 +0300

    was added header access-control-allow-origin for response body

--- WebServiceForManage/src/main/java/by/anabios13/courseworkmei/controllers/UserProfileController.java
+++ WebServiceForManage/src/main/java/by/anabios13/courseworkmei/controllers/UserProfileController.java
@@ -4,15 +4,19 @@ import by.anabios13.courseworkmei.dto.ProfileDTO;
 import by.anabios13.courseworkmei.services.DirectoryService;
 import by.anabios13.courseworkmei.services.NoteService;
 import by.anabios13.courseworkmei.services.PeopleService;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/profile")
+@CrossOrigin
 public class UserProfileController {
     private final PeopleService peopleService;
 
     private final DirectoryService directoryService;
     private final NoteService noteService;
+
 
     public UserProfileController(PeopleService peopleService, DirectoryService directoryService, NoteService noteService) {
         this.peopleService = peopleService;
@@ -22,10 +26,12 @@ public class UserProfileController {
 
 
     @GetMapping("/show/{id}")
-    public ProfileDTO show(@PathVariable("id") int id){
-
+    public ResponseEntity<ProfileDTO> show(@PathVariable("id") int id){
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("access-control-allow-origin", "*");
       ProfileDTO profileDTO= new ProfileDTO(peopleService.findOne(id));
-        return profileDTO;
+
+        return ResponseEntity.ok().headers(headers).body(profileDTO);
     }
 
 }
