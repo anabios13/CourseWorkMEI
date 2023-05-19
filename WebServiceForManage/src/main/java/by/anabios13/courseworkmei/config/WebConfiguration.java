59c9f07a106748ad6b2bb960a9e4284575386936
anabios13 <o.pinchykov@mail.ru>
2023-05-19 14:33:10 +0300

    was refactored class WebConfig and start_page.html

--- WebServiceForManage/src/main/java/by/anabios13/courseworkmei/config/WebConfiguration.java
+++ WebServiceForManage/src/main/java/by/anabios13/courseworkmei/config/WebConfiguration.java
@@ -8,7 +8,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfiguration implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/resources/**").addResourceLocations("classpath:/templates/");
-       // registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+        registry.addResourceHandler("/resources/**").addResourceLocations("classpath:/templates/profile/");
+        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+
     }
 }
