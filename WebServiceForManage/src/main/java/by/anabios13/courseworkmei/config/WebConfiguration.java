7605af5bca55a15862001b7458ec1d5c33d7b4ff
anabios13 <o.pinchykov@mail.ru>
2023-05-19 14:15:05 +0300

    was added class WebConfiguration

---
+++ WebServiceForManage/src/main/java/by/anabios13/courseworkmei/config/WebConfiguration.java
@@ -0,0 +1,14 @@
+package by.anabios13.courseworkmei.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**").addResourceLocations("classpath:/templates/");
+       // registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+    }
+}
